c3b8e7c8be7bead70de54df8a4eb718d40928ab2
Alumno <Alumno@LAB-BD10>
2017-09-14 13:30:17 -0500

    Examen

---
+++ ConvertidorMain/src/convertidormain/ConvertidorMain.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package convertidormain;
+
+/**
+ *
+ * @author Elliot
+ * @version 1.0
+ */
+public class ConvertidorMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /**
+         Constructor
+         */
+        Convertidor con = new Convertidor();
+        con.setVisible(true);
+    }
+    
+}
